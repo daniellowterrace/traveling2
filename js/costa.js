02ecade8ec45aa3327cb10537b524c5ce9e3cb12
spectaculor <mdekonty@verizon.net>
2016-08-02 22:28:00 +0000

    text

---
+++ js/costa.js
@@ -0,0 +1,11 @@
+
+
+
+
+   
+   $(document).ready(function(){
+        $(".ecotourism").click(function(){    
+            $("h3").replaceWith("Costa Rica is known for its ecotourism");
+    
+    });
+});
